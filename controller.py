c88c9dc2c3aa7f2acb8d81db5f393c933e24651f
vobegishev <vobegishev@gmail.com>
2023-04-11 08:25:45 +0300

    Add class phone_book_update

--- controller.py
+++ controller.py
@@ -1,34 +1,71 @@
-import model
-import view
+# import model
+# import view
+#
+#
+# def start():
+#     while True:
+#         pb = model.get_phone_book()
+#         choice = view.main_menu()
+#         match choice:
+#             case 1:
+#                 model.open_file()
+#                 view.show_message('Файл успешно открыт')
+#             case 2:
+#                 model.save_file()
+#                 view.show_message('Файл успешно сохранен')
+#             case 3:
+#                 view.show_contacts(pb, 'Телефонная книга пуста или не открыта')
+#             case 4:
+#                 contact = view.add_contact()
+#                 model.add_contact(contact)
+#             case 5:
+#                 if view.show_contacts(pb, 'Телефонная книга пуста или не открыта'):
+#                     index = view.input_index('Введите номер изменяемого контакта')
+#                     contact = view.change_contact(pb, index)
+#                     model.change_contact(contact, index)
+#                     view.show_message(f'Контакт {model.get_phone_book()[index-1].get("name")} успешно изменен!')
+#             case 6:
+#                 search = view.input_search('Введите искомый элемент: ')
+#                 result = model.find_contact(search)
+#                 view.show_contacts(result, 'Конаткты не найдены')
+#             case 7:
+#                 return
+#
+#
 
+import phone_book
+from phone_book import PhoneBook
 
-def start():
-    while True:
-        pb = model.get_phone_book()
-        choice = view.main_menu()
-        match choice:
-            case 1:
-                model.open_file()
-                view.show_message('Файл успешно открыт')
-            case 2:
-                model.save_file()
-                view.show_message('Файл успешно сохранен')
-            case 3:
-                view.show_contacts(pb, 'Телефонная книга пуста или не открыта')
-            case 4:
-                contact = view.add_contact()
-                model.add_contact(contact)
-            case 5:
-                if view.show_contacts(pb, 'Телефонная книга пуста или не открыта'):
-                    index = view.input_index('Введите номер изменяемого контакта')
-                    contact = view.change_contact(pb, index)
-                    model.change_contact(contact, index)
-                    view.show_message(f'Контакт {model.get_phone_book()[index-1].get("name")} успешно изменен!')
-            case 6:
-                search = view.input_search('Введите искомый элемент: ')
-                result = model.find_contact(search)
-                view.show_contacts(result, 'Конаткты не найдены')
-            case 7:
-                return
+pb = phone_book.PhoneBook('phone.txt')
+
+while True:
+    print(pb.main_menu())
+    choice = int(input('Выберите пункт меню: '))
+    match choice:
+        case 1:
+            print(pb)
+        case 2:
+            name = input('Введите имя: ')
+            phone = input('Введите номер телефона: ')
+            comment = input('Введите комментарий: ')
+            pb.new_contact(name, phone, comment)
+        case 3:
+            word = input('Введите поисковый запрос: ')
+            print(pb.search(word))
+        case 4:
+            print(pb)
+            index = int(input('Введите индекс изменяемого контакта: '))
+            name = input('Введите имя (или Enter, чтобы пропустить): ')
+            phone = input('Введите номер телефона (или Enter, чтобы пропустить): ')
+            comment = input('Введите комментарий (или Enter, чтобы пропустить): ')
+            pb.change(index-1, name, phone, comment)
+        case 5:
+            print(pb)
+            index = int(input('Введите индекс контакта, котороый хотите удалиить: '))
+            pb.delete(index-1)
+        case 6:
+            pb.save()
+        case 7:
+            break
 
 
--- main.py
+++ main.py
@@ -1,5 +1,32 @@
-import controller
+# import controller
+#
+# if __name__ == '__main__':
+#     controller.start()
+#
 
-if __name__ == '__main__':
-    controller.start()
+class Human:
+    def __init__(self, name: str, age: int, is_work: bool):
+        self.name = name
+        self.age = age
+        self.is_work = is_work
 
+    def greetings(self):
+        print(f'{self.name} говорит "Привет"')
+
+    def good_bye(self, name: str):
+        print(f'{self.name} прощается с {name}')
+
+    def __str__(self):
+        return f'Это человек с именем {self.name}, возраста {self.age}'
+
+
+stone = Human('Кирилл', 38, True)
+rudolf = Human('Рудольф', 18, True )
+
+print(stone.name)
+stone.greetings()
+rudolf.greetings()
+stone.good_bye('Рудольфом')
+rudolf.good_bye('Кириллом')
+print(stone)
+print(rudolf)
